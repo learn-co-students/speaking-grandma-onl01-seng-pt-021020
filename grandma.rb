a21e0ec2069bd6a6db1056063e991780be278b5f
excellent-revision-4527 <tjcroley@outlook.com>
2020-02-13 23:08:50 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,7 @@
-# Write a speak_to_grandma method.
+phrase = "I LOVE YOU GRANDMA!"
+def speak_to_grandma
+  if 
+end
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
