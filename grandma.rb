d94a7d6d3a3d495e5ac3c1d7b310918b7760418f
Gilberto Ocampo <ocampo@gate.net>
2020-02-14 22:51:52 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -4,6 +4,19 @@
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
+def speak_to_grandma(phrase)
+
+if phrase == 'I LOVE YOU GRANDMA!'
+  'I LOVE YOU TOO PUMPKIN!'
+elsif  phrase != phrase.upcase
+  'HUH?! SPEAK UP, SONNY!'
+else
+  'NO, NOT SINCE 1938!'
+end
+
+end  
+  
+
 # If you shout, she can hear you (or at least she thinks so) 
 # and yells back
 
